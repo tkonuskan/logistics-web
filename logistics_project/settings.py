b15ef5e018adb76704d2c6695f7aaaab573f2f51
tkonuskan <tkonuskan925@gmail.com>
2026-05-02 00:28:13 +0300

    KAN-10: settings.py tam yapılandırma eklendi

--- logistics_project/settings.py
+++ logistics_project/settings.py
@@ -1,8 +1,15 @@
-SECRET_KEY = 'django-insecure-1234567890abcdef!@#$%'
+import os
+from pathlib import Path
+
+# Proje kök dizini
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Güvenlik ayarları
+SECRET_KEY = 'django-insecure-1234567890abcdef!@#$%'  # Burayı kendine göre değiştirebilirsin
 DEBUG = True
+ALLOWED_HOSTS = ['*']  # Render için tüm hostlara izin ver
 
-ALLOWED_HOSTS = ['*']
-
+# Uygulamalar
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -15,6 +22,70 @@ INSTALLED_APPS = [
     'users',
 ]
 
+# Middleware
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# URL ayarı
+ROOT_URLCONF = 'logistics_project.urls'
+
+# Template ayarı
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / "templates"],  # login.html buradan yüklenir
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# WSGI
+WSGI_APPLICATION = 'logistics_project.wsgi.application'
+
+# Veritabanı (şimdilik SQLite)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
+}
+
+# Parola validasyonu
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+]
+
+# Dil ve zaman
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
+# Statik dosyalar
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# Varsayılan primary key
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# REST Framework ayarı
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
