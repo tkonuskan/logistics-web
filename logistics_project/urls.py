d91e2f46ffe269e5be3fe3568338fea3a52bede8
tkonuskan <tkonuskan925@gmail.com>
2026-05-02 00:39:28 +0300

    Güncelleme urls.py

--- logistics_project/urls.py
+++ logistics_project/urls.py
@@ -1,7 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import home_page, login_page
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('users.urls')),
+    path('', home_page, name='home_page'),  # kök URL
+    path('login/', login_page, name='login_page'),
+    path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
