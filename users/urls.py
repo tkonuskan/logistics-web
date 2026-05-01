718c4f8a91c9e745aa0c87ebc155bf35bc056fa7
tkonuskan <tkonuskan925@gmail.com>
2026-05-01 23:57:27 +0300

    KAN-10: users/urls.py dosyası eklendi

---
+++ users/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
