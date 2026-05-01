61ee92578f6f9495ea53fd0fac60445defdbc59f
tkonuskan <tkonuskan925@gmail.com>
2026-05-02 01:17:33 +0300

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,9 +1,2 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
-
-class CustomUser(AbstractUser):
-    role = models.CharField(max_length=20, choices=[
-        ("admin", "Admin"),
-        ("driver", "Driver"),
-        ("staff", "Staff")
-    ], default="staff")
+# Bu uygulamada özel kullanıcı modeli kullanılmıyor
